3a37ae60594b6d9fab5f817d9c51da064381ea66
dencharski <dencharski@yandex.ru>
2023-06-20 19:01:34 +0200

    -add RecyclerView

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
+import com.example.playlistmaker.SearchActivity.SearchActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/playlistmaker/SearchActivity/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity/SearchActivity.kt
@@ -1,4 +1,4 @@
-package com.example.playlistmaker
+package com.example.playlistmaker.SearchActivity
 
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
@@ -10,7 +10,9 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
-import androidx.core.widget.addTextChangedListener
+import androidx.recyclerview.widget.RecyclerView
+import com.example.playlistmaker.R
+import com.example.playlistmaker.Track
 
 class SearchActivity : AppCompatActivity() {
 
@@ -18,6 +20,42 @@ class SearchActivity : AppCompatActivity() {
     private var clearButton: ImageView? = null
     private var goBackButton: ImageView? = null
     private val key: String = "key"
+    private var trackListAdapter: TrackListAdapter? = null
+    private var recyclerViewTrackList: RecyclerView? = null
+
+    private val trackList = arrayListOf<Track>(
+        Track(
+            trackName = "Smells Like Teen Spirit",
+            artistName = "Nirvana",
+            trackTime = "5:01",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Billie Jean",
+            artistName = "Michael Jackson",
+            trackTime = "4:35",
+            artworkUrl100 = " https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Stayin' Alive",
+            artistName = "Bee Gees",
+            trackTime = "4:10",
+            artworkUrl100 = "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Whole Lotta Love",
+            artistName = "Led Zeppelin",
+            trackTime = "5:33",
+            artworkUrl100 = "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
+        ),
+        Track(
+            trackName = "Sweet Child O'Mine",
+            artistName = "Guns N' Roses",
+            trackTime = "5:03",
+            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        )
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
@@ -25,6 +63,10 @@ class SearchActivity : AppCompatActivity() {
         editTextSearch = findViewById<EditText>(R.id.edit_text_search)
         clearButton = findViewById<ImageView>(R.id.image_view_clear)
         goBackButton = findViewById(R.id.image_view_back_arrow)
+        recyclerViewTrackList=findViewById(R.id.recycler_view_track_list)
+
+        trackListAdapter=TrackListAdapter(trackList)
+        recyclerViewTrackList?.adapter=trackListAdapter
 
         clearButton?.setOnClickListener {
             editTextSearch?.setText("")
